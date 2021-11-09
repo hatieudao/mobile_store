892a91669340b3f840abe894cdf25d335f68d2f5
hatieudao <froyal2kk1@gmail.com>
2021-11-09 08:33:42 +0700

    config

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = process.env.PORT || 5000;
+const cookieParser = require('cookie-parser')
+const logger = require('morgan')
+const homeRoute = require('./routes/home.route')
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.set("views", "./views")
+app.set('view engine', 'hbs')
+
+app.use(logger('dev'))
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use(cookieParser())
+app.use(express.static('public'))
+
+app.use(homeRoute)
+
+app.get('/', homeRoute)
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
