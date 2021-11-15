e003ba3b14189f091285f6899bcbcd8376af9f33
hatieudao <froyal2kk1@gmail.com>
2021-11-15 21:47:30 +0700

    fix some bug

--- routes/shop-page.route.js
+++ routes/shop-page.route.js
@@ -2,8 +2,8 @@ const express = require('express');
 const router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('shop-page', { title: 'Shop Page' });
+router.get('/', function (req, res, next) {
+  res.render('shop', { title: 'Shop Page' });
 });
 
 module.exports = router;
