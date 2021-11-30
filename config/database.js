e22ecfb1a8dcb10fb2b79ad63685b09ffbcff9b4
hatieudao <froyal2kk1@gmail.com>
2021-11-30 21:46:54 +0700

    config DB

--- config/database.js
+++ config/database.js
@@ -1,6 +1,13 @@
 const { Sequelize } = require('sequelize');
 
-module.exports = new Sequelize(process.env.DB_DATABASE, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
+const dbDev = new Sequelize(process.env.DB_DATABASE, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
   host: 'localhost',
   dialect: 'postgres'
 });
+const dbProduct = new Sequelize(process.env.DATABASE_URL)
+
+const db = (process.env.NODE_ENV === 'production' ? dbProduct : dbDev)
+
+console.log(`This app lanch in ${process.env.NODE_ENV} environment`)
+
+module.exports = db
