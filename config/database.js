c7d4969067aaec24476eda2331cc2ea2aa58b9be
hatieudao <froyal2kk1@gmail.com>
2021-11-30 21:51:11 +0700

    ssl true

--- config/database.js
+++ config/database.js
@@ -4,10 +4,15 @@ const dbDev = new Sequelize(process.env.DB_DATABASE, process.env.DB_USERNAME, pr
   host: 'localhost',
   dialect: 'postgres'
 });
-const dbProduct = new Sequelize(process.env.DATABASE_URL)
+const dbProduct = new Sequelize(process.env.DATABASE_URL, {
+  dialectOptions: {
+    ssl: true
+  }
+})
 
 const db = (process.env.NODE_ENV === 'production' ? dbProduct : dbDev)
 
 console.log(`This app lanch in ${process.env.NODE_ENV} environment`)
+
 
 module.exports = db
