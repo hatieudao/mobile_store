221b3e2b4a81d9cb3310cdb64b50c7382cba0d3a
hatieudao <froyal2kk1@gmail.com>
2021-11-30 22:03:46 +0700

    ssl config

--- config/database.js
+++ config/database.js
@@ -5,8 +5,9 @@ const dbDev = new Sequelize(process.env.DB_DATABASE, process.env.DB_USERNAME, pr
   dialect: 'postgres'
 });
 const dbProduct = new Sequelize(process.env.HDB_DATABASE, process.env.HDB_USERNAME, process.env.HDB_PASSWORD, {
-  host: process.env.HOST,
+  host: process.env.HDB_HOST,
   dialect: 'postgres',
+  port: process.env.HDB_PORT,
   dialectOptions: {
     ssl: {
       require: true,
