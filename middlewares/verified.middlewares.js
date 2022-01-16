351c33d7ec4c964daefc8441fcda96b3fcfa298b
hatieudao <froyal2kk1@gmail.com>
2022-01-16 22:54:22 +0700

    add verify email

--- middlewares/verified.middlewares.js
+++ middlewares/verified.middlewares.js
@@ -1,4 +1,6 @@
 exports.isVerified = (req, res, next) => {
   if (req.user?.status === "unlock") next();
-  res.render('verify-request', { link: `${process.env.DOMAIN}/verify`, layout: 'emptyLayout' })
+  else {
+    res.render('verify-request', { link: `${process.env.DOMAIN}/verify`, layout: 'emptyLayout' })
+  }
 }
