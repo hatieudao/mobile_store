eb594cc47bdffdf1846c86e168f9c54cdf0a425c
hatieudao <froyal2kk1@gmail.com>
2022-01-16 21:54:04 +0700

    add logo

--- middlewares/verified.middlewares.js
+++ middlewares/verified.middlewares.js
@@ -1,9 +1,7 @@
 const { sendEmailVerify } = require('../utils/sendEmailVerify');
 const { createToken } = require('../utils/createToken');
 exports.isVerified = (req, res, next) => {
-  // if (req.user?.status === "unlock") next();
-  // sendEmailVerify('trumbien2001@gmail.com');
-  // res.send("Please verify your email");
-  console.log(req.user);
-  next();
+  if (req.user?.status === "unlock") next();
+  console.log(req.user)
+  res.render('verify-request', { link: `${process.env.DOMAIN}/verify`, layout: 'emptyLayout' })
 }
