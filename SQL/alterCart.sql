d236a1dd9d826a0fc849fa5e3ceb411cc5ab230d
hatieudao <froyal2kk1@gmail.com>
2022-01-17 20:17:21 +0700

    reolve conflict

--- SQL/alterCart.sql
+++
@@ -1,4 +0,0 @@
-ALTER TABLE cart ADD COLUMN address varchar(300);
-ALTER TABLE cart ADD COLUMN phone varchar(20);
-ALTER TABLE cart_details ADD COLUMN quantity integer;
-ALTER TABLE comments ADD COLUMN parent_id integer;
