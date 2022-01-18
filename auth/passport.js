19511e6476b144898e53c8cd5258b7f382d1eeff
hatieudao <froyal2kk1@gmail.com>
2022-01-18 21:02:25 +0700

    status null can login

--- auth/passport.js
+++ auth/passport.js
@@ -1,31 +1,34 @@
 const passport = require('passport')
-    , LocalStrategy = require('passport-local').Strategy;
+  , LocalStrategy = require('passport-local').Strategy;
 const bcrypt = require("bcryptjs");
 
-const {models} = require("../models");
+const { models } = require("../models");
 const authService = require("../services/authService");
 
 passport.use(new LocalStrategy(
-    async function (username, password, done) {
-        try {
-            //const user = await models.users.findOne({where: {username}, raw: true});
-            const user = await authService.getUserbyUsername(username);
-            if (!user) {
-                return done(null, false, {message: 'Incorrect username.'});
-            }
-            const match = await validPassword(user, password);
-            if (!match) {
-                return done(null, false, {message: 'Incorrect password.'});
-            }
-            return done(null, user);
-        } catch (err) {
-            return done(err);
-        }
+  async function (username, password, done) {
+    try {
+      //const user = await models.users.findOne({where: {username}, raw: true});
+      const user = await authService.getUserbyUsername(username);
+      if (!user) {
+        return done(null, false, { message: 'Incorrect username.' });
+      }
+      const match = await validPassword(user, password);
+      if (!match) {
+        return done(null, false, { message: 'Incorrect password.' });
+      }
+      if (user.status === 'lock') {
+        return done(null, false, { message: 'Incorrect username.' });
+      }
+      return done(null, user);
+    } catch (err) {
+      return done(err);
     }
+  }
 ));
 
 async function validPassword(user, password) {
-    return bcrypt.compare(password, user.password);
+  return bcrypt.compare(password, user.password);
 }
 
 passport.serializeUser(function (user, done) {
@@ -35,7 +38,7 @@ passport.serializeUser(function (user, done) {
 });
 
 passport.deserializeUser(function (user, done) {
-    return done(null, user);
+  return done(null, user);
 });
 
 module.exports = passport;
